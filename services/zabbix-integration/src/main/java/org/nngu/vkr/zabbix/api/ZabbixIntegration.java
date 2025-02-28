97291c8c31c7876c3d20a4f8ff9c99fee6b5a7fb
MoXuToS <moxutosic@gmail.com>
2025-02-28 20:38:48 +0300

    Файлик потеряли)

---
+++ services/zabbix-integration/src/main/java/org/nngu/vkr/zabbix/api/ZabbixIntegration.java
@@ -0,0 +1,12 @@
+package org.nngu.vkr.zabbix.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabbixIntegration {
+    public static void main(String[] args) {
+        SpringApplication.run(ZabbixIntegration.class, args);
+    }
+}
