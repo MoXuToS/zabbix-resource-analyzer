4aba1a68b576ca11bd4154919cb86ecf405ccaf8
MoXuToS <moxutosic@gmail.com>
2025-03-08 22:19:10 +0300

    @feature added Metric provider main class

---
+++ services/metricProvider/src/main/java/org/nngu/vkr/metric/provider/MetricProvider.java
@@ -0,0 +1,11 @@
+package org.nngu.vkr.metric.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricProvider {
+    public static void main(String[] args){
+        SpringApplication.run(MetricProvider.class, args);
+    }
+}
